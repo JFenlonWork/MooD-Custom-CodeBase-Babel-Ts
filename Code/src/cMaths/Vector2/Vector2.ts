d7ae69f951e7cfa43596a5335a6e2e82e6ead595
JFenlonWork <jfenlon@caci.co.uk>
2022-02-28 16:45:57 +0000

    Upload Initial Workspace Setup

---
+++ Code/src/cMaths/Vector2/Vector2.ts
@@ -0,0 +1,16 @@
+/*
+	Title:
+		Vector2
+	
+	Description:
+		Simple implementation for a vector2 (float, float)
+*/
+
+export {test as lolol}
+
+/** */
+const test = 1;
+
+class Vector2 {
+    //Constructor
+}
