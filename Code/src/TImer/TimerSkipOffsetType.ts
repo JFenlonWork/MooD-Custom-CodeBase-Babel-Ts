a5fc0f7c9dcbac7d9f1c8b192466305d5d30d9b2
JFenlonWork <jfenlon@caci.co.uk>
2022-03-22 01:33:26 +0000

    Removed un-needed files.

--- Code/src/TImer/TimerSkipOffsetType.ts
+++
@@ -1,7 +0,0 @@
-/** Enum representing a offset skip type of a timer.
- */
- export enum TimerSkipOffsetType {
-    NoSkip = 0,
-    SkipAnyIncludingInstantLoops = 1,
-    SkipExcludingInstantLoops = 2
- }
