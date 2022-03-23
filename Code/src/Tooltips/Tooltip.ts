2ad71717f10c677ae8f7ad92042ba911b93b2f88
JFenlonWork <jfenlon@caci.co.uk>
2022-03-23 05:53:20 +0000

    Added initial tooltip code

---
+++ Code/src/Tooltips/Tooltip.ts
@@ -0,0 +1,5 @@
+/** Class representing a Tooltip that can be added to pre-existing HTML.
+ */
+ export class Tooltip {
+    
+ }
---
+++ Code/src/Tooltips/TooltipController.ts
@@ -0,0 +1,62 @@
+import { MultiKeyMap } from "../Utilities/MultiKeyMap";
+import { UniqueID } from "../Utilities/UniqueID";
+import { Tooltip } from "./Tooltip";
+
+/** Class representing a any utility functions or variables that help Tooltips.
+ */
+ export class TooltipController {
+    
+    //** Store a singleton of TooltipController to assure only one exists */
+    private static _instance: TooltipController;
+    public static get Instance(): TooltipController {
+        return this._instance || (this._instance = new this());
+    }
+
+
+    /**
+	 * Return this or singleton instance of TooltipController
+	 */
+    private constructor() {
+        if (TooltipController._instance) return TooltipController._instance;
+        TooltipController._instance = this;
+    }
+
+    //** Store an incrementing variable to ensure unique IDs*/
+    private _uniqueID: UniqueID = new UniqueID();
+    public get uniqueID(): UniqueID {
+        if (this != TooltipController.Instance) return TooltipController.Instance.uniqueID;
+        return this._uniqueID;
+    }
+
+    //** Store all references to Tooltips to allow searching*/
+    private _tooltips: MultiKeyMap<String | Number, Tooltip> = new MultiKeyMap();
+    public get tooltips(): MultiKeyMap<String | Number, Tooltip> {
+        if (this != TooltipController.Instance) return TooltipController.Instance.tooltips;
+        return this._tooltips;
+    }
+
+
+
+    /**
+	 * Searches for and returns a Tooltip with a name parameter
+	 * @param  {string} name - The Tooltip to return
+	 */
+     public static getTooltip(name: string): Tooltip | undefined {
+        if (typeof(name) != "string") { console.error("Trying to get a Tooltip with an invalid name: ", name); return; }
+        return this.Instance.tooltips.getValue(name);
+    }
+
+    /**
+	 * Searches for and returns a Tooltip with a id parameter
+	 * @param  {number} id - The Tooltip to return
+	 */
+    public static getTooltipFromID(id: number): Tooltip | undefined {
+        if (typeof(id) != "number") { console.error("Trying to get a Tooltip with an invalid id: ", id); return; }
+        return this.Instance.tooltips.getValue(id);
+    }
+
+
+
+    
+
+ }
