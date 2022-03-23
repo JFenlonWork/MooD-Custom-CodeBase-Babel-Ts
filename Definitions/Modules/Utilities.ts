abe47770432ad0e1bac579af315d6df9b5bd0cbc
JFenlonWork <jfenlon@caci.co.uk>
2022-03-23 05:52:03 +0000

    Update webpack imports

--- Definitions/Modules/Utilities.ts
+++ Definitions/Modules/Utilities.ts
@@ -1,3 +1,5 @@
 import { MultiKeyMap } from "../../Code/src/Utilities/MultiKeyMap"
+import { UniqueID } from "../../Code/src/Utilities/UniqueID"
+import { CompareTypes } from "../../Code/src/Utilities/CompareTypes"
 
-export { MultiKeyMap };
+export { MultiKeyMap, UniqueID, CompareTypes };
