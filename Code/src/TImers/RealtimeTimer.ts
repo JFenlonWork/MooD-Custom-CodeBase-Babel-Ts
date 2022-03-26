942b904830a181efd331e3531aef7045da94318c
JFenlonWork <jfenlon@caci.co.uk>
2022-03-26 23:44:18 +0000

    Fixed few errors with RealtimeTimer

--- Code/src/TImers/RealtimeTimer.ts
+++ Code/src/TImers/RealtimeTimer.ts
@@ -7,45 +7,51 @@ import { TimerSkipOffsetType } from "./TimerSkipOffsetType";
  */
  export class RealtimeTimer extends Timer {
 
-    //** Handles any custom events required by this Timer*/
+    //** Handles any custom events required by this RealtimeTimer*/
     private _realtimeEvents: PubSub = new PubSub();
-    public get realtimeEvents(): PubSub {
+    public override get events(): PubSub {
         return this._realtimeEvents;
     }
-    
+
+    //** Handles if this Timer should destroy itself once it stops*/
     private _destroyOnStop: boolean = true;
     public get destroyOnStop(): boolean {
         return this._destroyOnStop;
     }
 
     public set destroyOnStop(destroyOnStop: boolean) {
+        if (typeof(destroyOnStop) != "boolean") { console.warn("Trying to modify a RealtimeTimer's destroyOnStop with an invalid input: ", destroyOnStop); return; }
         this._destroyOnStop = destroyOnStop;
     }
 
     /**
 	 * Create a RealtimeTimer
 	 * @param  {string} name - The name of the timer
-	 * @param  {Function[]} callbacks - The callbacks listening to this timer
+	 * @param  {Array<Function>} callbacks - The callbacks listening to this timer
 	 * @param  {number} timerInterval - The time between each loop on this timer
 	 * @param  {boolean} startOnCreation - Determines if this timer should start running after creation
 	 * @param  {number} timerRunTime - The total time for this timer to run 
 	 * @param  {boolean} destroyOnStop - Determines if a timers should destroy itself once it recieves a single stop command
 	 */
-    constructor (name: string, callbacks: Function[] = [], startOnCreation:boolean = true, timerRunTime:number = Number.MAX_SAFE_INTEGER, destroyOnStop: boolean = true) {
+    constructor (name: string, callbacks: Array<Function> = [], startOnCreation:boolean = true, timerRunTime:number = Number.MAX_SAFE_INTEGER, destroyOnStop: boolean = true) {
         super(name, 10, [], startOnCreation, timerRunTime, true, TimerSkipOffsetType.NoSkip);
         this.destroyOnStop = destroyOnStop;
         super.events.subscribe("loopCompletion", () => { this.events.publish("loopCompletion"); });
-        this.realtimeEvents.subscribe("loopCompletion", callbacks);
-        this.realtimeEvents.subscribe("RealtimeResponse", this.listenToResponse);
+        this.events.subscribe("loopCompletion", callbacks);
+
+        let _this = this;
+        this.events.subscribe("response", (...args: any[]) => { return this.listenToResponse.call(_this, ...args) });
     }
 
+    //** Handles listening to responses from callbacks to see if this timer should stop*/
     private listenToResponse(...args: any[]) {
-        if (args.length == 0 || typeof(args[0]) != "boolean") { console.warn("Trying to respond to RealtimeTimer with invalid parameters: ", args); return; }
-        if (args[0] == false) {
+        if (args.length != 2 || typeof(args[1]) != "boolean") { console.warn("Trying to respond to RealtimeTimer with invalid parameters: ", args); return; }
+
+        if (args[1] == false) {
             if (this.destroyOnStop) {
                 this.destroy();
             } else {
-               this.stop();
+                this.stop();
             }
         }        
     }
