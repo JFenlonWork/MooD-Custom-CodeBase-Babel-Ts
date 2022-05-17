446a0b9179c214d27504f8e9c0771ea7263df853
JFenlonWork <jfenlon@caci.co.uk>
2022-05-17 05:36:03 +0100

    Update JQueryStatic.d.ts
    Unnecessary definition

--- Definitions/JQueryStatic.d.ts
+++ Definitions/JQueryStatic.d.ts
@@ -1,3 +1,3 @@
 interface JQueryStatic {
-    <TElement extends Element = HTMLElement>(selector: string | HTMLElement, context?: Element | Document | JQuery | JQuery.Selector): JQuery<TElement>;
+    //<TElement extends Element = HTMLElement>(selector: string | HTMLElement, context?: Element | Document | JQuery | JQuery.Selector): JQuery<HTMLElement>;
 }
