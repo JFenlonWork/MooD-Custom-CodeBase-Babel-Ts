f7991023031adda57aa30d56c97da9b926ce85c2
JFenlonWork <jfenlon@caci.co.uk>
2022-05-17 05:32:51 +0100

    Setup Jasmine Webpack Config

---
+++ spec/support/jasmineWebpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const glob = require("glob");
+
+module.exports = {
+    entry: glob.sync("./spec/support/Modules/**/*.ts"),
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                sideEffects: true,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        configFile: path.resolve(__dirname, '../../.babelrc')
+                    }
+                }
+            },
+        ],
+    },
+    externals: {
+        jquery: 'jQuery',
+        jasmine: 'jasmine'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        library: 'TestExport',
+        libraryTarget: 'window',
+        filename: 'jasmineTestBundle.js',
+        path: path.resolve(__dirname, ''),
+    },
+    optimization: {
+        minimize: false
+    },
+    target: 'node'
+  };
