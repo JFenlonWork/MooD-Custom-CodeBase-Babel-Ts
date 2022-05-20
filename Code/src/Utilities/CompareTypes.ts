66d90f3c85fef73fe506aad818e095519ed03bb7
JFenlonWork <jfenlon@caci.co.uk>
2022-05-21 00:09:34 +0100

    Modified compare
    Added callable static "type" function that remove need to type toString call

--- Code/src/Utilities/CompareTypes.ts
+++ Code/src/Utilities/CompareTypes.ts
@@ -8,7 +8,7 @@ export class CompareTypes {
      * @param  {any} b - The value to compare to
     */
     public static same(a: any, b: any): boolean {
-        return Object.prototype.toString.call(a) == Object.prototype.toString.call(b);
+        return CompareTypes.type(a) == CompareTypes.type(b);
     }
 
     /**
@@ -18,6 +18,14 @@ export class CompareTypes {
     */
     public static different(a: any, b: any): boolean {
         return !CompareTypes.same(a, b);
+    }
+
+    /**
+     * Return the type of a parameter
+     * @param  {any} a - The value to get the type of
+     */
+    public static type(a: any): string {
+        return Object.prototype.toString.call(a).slice(8, -1);
     }
 
 }
